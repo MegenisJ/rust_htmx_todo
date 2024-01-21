5f19405a22412b036f51599de18b3f5868eef3a5
MegenisJ <j.a.megenis@gmail.com>
2024-01-21 23:27:52 +0000

    style improvements

--- src/todo/mod.rs
+++ src/todo/mod.rs
@@ -19,17 +19,17 @@ pub struct NewTodo {
 pub fn TodoForm(cx: Scope, todos: Vec<Todo>, route: &'static str) -> impl IntoView {
     // create user interfaces with the declarative `view!` macro
     return view! { cx,
-        <form hx-post="{route}"
-            hx-target="#todos"
-            hx-swap="afterbegin"
-            hx-trigger="submit"
-            class = "container mx-auto px-4 justify-items-center flex flex-col" >
-            <h3>Create a new todo item</h3>
-            <input name = "title" type="text" placeholder="Title" />
-            <input name = "extras" type="text" placeholder="Detail" />
-            <button type="submit">"Add new todo"</button>
-            <Todos todos=todos />
-        </form>
+            <form hx-post="{route}"
+                hx-target="#todos"
+                hx-swap="afterbegin"
+                hx-trigger="submit"
+                class = "mx-auto mt-10 grid max-w-2xl gap-x-4 gap-y-4 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4" >
+                <h3>Create a new todo item</h3>
+                <input name = "title" type="text" placeholder="Title" />
+                <input name = "extras" type="text" placeholder="Detail" />
+                <button type="submit">"Add new todo"</button>
+                <Todos todos=todos />
+            </form>
     };
 }
 
@@ -64,7 +64,7 @@ pub fn Todos(cx: Scope, todos: Vec<Todo>) -> impl IntoView {
 pub fn Todo(cx: Scope, todo: Todo) -> impl IntoView {
     let id = todo.id;
     return view! {cx,
-        <li class = "flex flex-col justify-center items-center bg-indigo-500 border rounded-lg">
+        <li class = "mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             <div>title: {todo.title}</div>
             <div>extra detail: {todo.extras}</div>
             <div>completed: {todo.completed}</div>
